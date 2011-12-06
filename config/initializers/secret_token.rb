4d8c67771273f9f7f27703ab1ac7df4bb5d60397
Jonas Myrlund <myrlund@gmail.com>
2011-12-06 19:45:06 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trackr::Application.config.secret_token = '4170d8a47a2a25f6c8f29641921f806d98dcf3618b41551acec8b11266d2c79d2e782824b31f398a62a229f2cd9a92ba478b0642035c981db5b2090b3f449928'
